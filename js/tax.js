6719894173b163a5a063e2408943462112121b70
TrailBl4zer <31038145+TrailBl4zer@users.noreply.github.com>
2017-08-18 11:07:52 +0200

    1

---
+++ js/tax.js
@@ -0,0 +1,42 @@
+
+var price, vat, gross;
+
+/* Wipe textfields on page load/refresh */
+
+document.getElementsByName("price")[0].value = "";
+
+/* Handler */
+
+function initCalc(){
+getValues();
+setFinalValue(calculateTax(price, vat, gross));
+}
+
+/* Get necessary values from input */
+
+function getValues(){
+  price = document.getElementsByName("price")[0].value;
+  vat = document.getElementsByName("vat")[0].value;
+  gross = document.getElementsByName("gross")[0].checked;
+
+}
+
+/* Main TAX calculation with variables */
+
+function calculateTax(value, tax, isGross){
+  let final;
+  if (isGross){
+    final = 100 * parseFloat(value) / (100 + parseFloat(tax));
+  }else {
+    final = (100 + parseFloat(tax)) * parseFloat(value) / 100;
+  }
+  return final.toFixed(2);
+}
+
+/* Output data */
+
+function setFinalValue(finalPrice){
+  document.getElementById("final").innerHTML = finalPrice + " PLN";
+}
+
+/* IE radio failsafe */
