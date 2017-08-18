a854df53f11c304e07bebaace33825bc146f8e4b
TrailBl4zer <31038145+TrailBl4zer@users.noreply.github.com>
2017-08-18 18:44:12 +0200

    Fixed tax.js

--- js/tax.js
+++ js/tax.js
@@ -1,39 +1,34 @@
 
 var price, vat, gross;
 
-<<<<<<< HEAD
 /* Wipe textfields on page load/refresh */
 
 document.getElementsByName("price")[0].value = "";
 
 /* Handler */
 
-=======
+
 document.getElementsByName("price")[0].value = "";
 
->>>>>>> master
 function initCalc(){
 getValues();
 setFinalValue(calculateTax(price, vat, gross));
 }
 
-<<<<<<< HEAD
 /* Get necessary values from input */
 
-=======
->>>>>>> master
+
 function getValues(){
   price = document.getElementsByName("price")[0].value;
   vat = document.getElementsByName("vat")[0].value;
   gross = document.getElementsByName("gross")[0].checked;
 
 }
-<<<<<<< HEAD
+
 
 /* Main TAX calculation with variables */
 
-=======
->>>>>>> master
+
 function calculateTax(value, tax, isGross){
   let final;
   if (isGross){
@@ -44,7 +39,7 @@ function calculateTax(value, tax, isGross){
   return final.toFixed(2);
 }
 
-<<<<<<< HEAD
+
 /* Output data */
 
 function setFinalValue(finalPrice){
@@ -52,8 +47,7 @@ function setFinalValue(finalPrice){
 }
 
 /* IE radio failsafe */
-=======
+
 function setFinalValue(finalPrice){
   document.getElementById("final").innerHTML = finalPrice + " PLN";
 }
->>>>>>> master
